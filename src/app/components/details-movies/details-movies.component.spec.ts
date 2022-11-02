1166cfeb33a68fbf388c7e24105928dc0a3838b7
José Luis <jlholgado81@googlemail.com>
2022-11-02 12:52:08 +0100

    final commit

---
+++ src/app/components/details-movies/details-movies.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DetailsMoviesComponent } from './details-movies.component';
+
+describe('DetailsMoviesComponent', () => {
+  let component: DetailsMoviesComponent;
+  let fixture: ComponentFixture<DetailsMoviesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DetailsMoviesComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DetailsMoviesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, OnChanges, AfterContentInit, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class DetailsComponent implements OnInit, OnChanges, AfterContentInit, AfterViewInit {
+
+  constructor() { }
+
+
+  ngOnChanges() {
+    console.log('ngOnChanges called');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+  }
+}
